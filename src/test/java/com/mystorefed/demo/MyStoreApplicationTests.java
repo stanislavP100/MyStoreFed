80c206478dbacb98354e160d79965c334eacc4c3
stanislavP100 <stanislav_p100@ukr.net>
2020-08-16 08:29:24 +0300

    Initial commit

---
+++ src/test/java/com/mystorefed/demo/MyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystorefed.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
